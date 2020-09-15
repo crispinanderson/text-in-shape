73617273f1408c9a8167418b45e47c544069c93b
Crispin Anderson <crispin.anderson@gmail.com>
2020-09-15 11:34:01 +0100

    use updated calcTextJustification

--- src/createRenderData.js
+++ src/createRenderData.js
@@ -1,16 +1,17 @@
 import { svgElementToCanvas, destroyCanvas } from "./svgElementToCanvas";
 import { calcLineAlignment } from "./calcLineAlignment";
-import { calcLineJustification } from "./calcLineJustification";
+import { calcTextJustification } from "./calcTextJustification";
 import { getStyledFontDimensions } from "./getStyledFontDimensions";
-import { calcLinePositions } from "./calcLineData";
+import { calcLinePositions } from "./calcLinePositions";
+import { addTextContentToLineData } from "./addTextContentToLineData";
 
 export const createRenderData = (text, svgElement, options) => {
     const context = svgElementToCanvas(svgElement);
     const fontDims = getStyledFontDimensions(svgElement, options);
     const lineData = calcLinePositions({ ...fontDims, context, svgElement, options, text });
     const lineDataWithText = addTextContentToLineData({ ...fontDims, text, lineData });
-    const lineDataAligned = calcLineAlignment(options, thisLine);
-    const lineDataJustified = options.lineJustify ? lineDataAligned.map((thisLine) => calcLineJustification(options, thisLine)) : lineDataAligned;
+    const lineDataAligned = calcLineAlignment(options, lineDataWithText);
+    const lineDataJustified = options.justifyText ? calcTextJustification(lineDataAligned) : lineDataAligned;
     destroyCanvas();
 
     return lineDataJustified;
