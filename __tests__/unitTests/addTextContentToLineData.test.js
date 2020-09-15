e97fac106909cd7d92a14ddd858f3fe041b1265b
Crispin Anderson <crispin.anderson@gmail.com>
2020-09-15 08:57:48 +0100

    test setup for mockLineData

--- __tests__/unitTests/addTextContentToLineData.test.js
+++ __tests__/unitTests/addTextContentToLineData.test.js
@@ -2,11 +2,8 @@ import { addTextContentToLineData } from '../../src/addTextContentToLineData';
 import { mockLineData } from '../utils/mockLineData';
 
 describe('addTextContentToLineData - ', () => {
-    const options = {
-        lineHeigt: 55,
-        charWidth: 18
-    }
-    const {lineData , charWidth } = options;
+    const lineHeight = 55;
+    const charWidth = 18;
     const longText = `Lorem ipsum dolor sit amet, nec ut dolorum hendrerit. 
 Ad novum nostro eum, mei no option voluptaria. 
 Senserit referrentur ullamcorper et sed, per semper timeam feugait id. 
@@ -14,14 +11,14 @@ In mea alia meliore, wisi justo his ne. Te essent eripuit appellantur eos. In in
 At homero soleat vocibus vim, causae referrentur comprehensam te mea. Ei duo fastidii complectitur, duo legendos euripidis no. Ea habeo invidunt vel. Et omnis probatus senserit eos, accumsan adipisci eum ut. Eu vel mandamus definitiones, usu no probo tempor, vel ad ignota imperdiet reprimique.`;
 
     test('An array of lineData is returned', () => {
-        let lineData = mockLineData(100, options);
+        let lineData = mockLineData({ length: 100, lineHeight });
         const text = longText;
         const dataWithText = addTextContentToLineData({ text, lineData, charWidth });
         expect(dataWithText.length > 0).toBe(true);
     })
 
     test('Every entry has a textContent and a textWidth property <= 500', () => {
-        let lineData = mockLineData(100, options);
+        let lineData = mockLineData({ length: 100, lineHeight });
         const text = longText;
         const dataWithText = addTextContentToLineData({ text, lineData, charWidth });
         dataWithText.forEach((lineData) => {
@@ -31,14 +28,14 @@ At homero soleat vocibus vim, causae referrentur comprehensam te mea. Ei duo fas
     })
 
     test('The last line is appended with ... when the text does not fit within the shape', () => {
-        let lineData = mockLineData(10, options);
+        let lineData = mockLineData({ length: 20, lineHeight });
         const text = longText;
         const dataWithText = addTextContentToLineData({ text, lineData, charWidth });
         expect(dataWithText.pop().textContent.slice(-3)).toBe('...');
     })
 
     test('An empty array is returned when an empty string is passed', () => {
-        let lineData = mockLineData(20, options);
+        let lineData = mockLineData({ length: 20, lineHeight });
         const text = '';
         const dataWithText = addTextContentToLineData({ text, lineData, charWidth });
         expect(dataWithText.length).toBe(0);
