a727bc4b6febee3df1148a207b5b0b1eeb2c5921
Crispin Anderson <crispin.anderson@gmail.com>
2020-09-17 09:06:08 +0100

    moved getLineHeight to utils

--- src/createRenderData.js
+++ src/createRenderData.js
@@ -1,15 +1,15 @@
 import { svgElementToCanvas, destroyCanvas } from "./svgElementToCanvas";
 import { calcLineAlignment } from "./calcLineAlignment";
 import { calcTextJustification } from "./calcTextJustification";
-import { getLineHeight } from "./getStyledFontDimensions";
 import { calcLinePositions } from "./calcLinePositions";
 import { addTextContentToLineData } from "./addTextContentToLineData";
+import { getLineHeight } from './utils';
 
 export const createRenderData = (text, svgElement, options) => {
     const context = svgElementToCanvas(svgElement);
-    const fontDims = getLineHeight(svgElement, options);
-    const lineData = calcLinePositions({ ...fontDims, context, svgElement, options, text });
-    const lineDataWithText = addTextContentToLineData({ ...fontDims, text, lineData, options });
+    const lineHeight = getLineHeight(options);
+    const lineData = calcLinePositions({ lineHeight, context, svgElement, options, text });
+    const lineDataWithText = addTextContentToLineData({ lineHeight, text, lineData, options });
     const lineDataAligned = calcLineAlignment(options, lineDataWithText);
     const lineDataJustified = options.justifyText ? calcTextJustification(lineDataAligned) : lineDataAligned;
     destroyCanvas();
