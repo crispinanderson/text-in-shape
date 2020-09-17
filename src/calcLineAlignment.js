fce59bdcdd6ab7c374e299abfedfd32e2f5c0fe0
Crispin Anderson <crispin.anderson@gmail.com>
2020-09-17 08:56:03 +0100

    removed debugger

--- src/calcLineAlignment.js
+++ src/calcLineAlignment.js
@@ -2,7 +2,6 @@ export const calcLineAlignment = (options, lineData) => {
     switch (options.align) {
         case 'center':
             return lineData.map((thisLine) => {
-                if (thisLine.textContent.includes('Ad novum nostro eum, mei no')) debugger
                 return { ...thisLine, x: thisLine.x + ((thisLine.width - thisLine.textWidth) / 2) }
             });
         case 'right':
