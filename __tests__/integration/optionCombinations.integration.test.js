759b2f7803318651c4e99095e6f5e43f9235a155
Crispin Anderson <crispin.anderson@gmail.com>
2020-09-19 08:39:25 +0100

    optionCombinations tests & screenshots

--- __tests__/integration/optionCombinations.integration.test.js
+++ __tests__/integration/optionCombinations.integration.test.js
@@ -33,13 +33,13 @@ describe('Option Combinations - ', () => {
 
     });
 
-    test('Padding and justifyText', async () => {
+    test('Padding and justifyText and align right --> alignment is ignored', async () => {
 
         const testPadding = async (p) => await page.evaluate((text, config, p) => {
 
             const options = {
                 ...config.options,
-                align: 'left',
+                align: 'right',
                 padding: 50,
                 justifyText: true,
                 style: {
@@ -79,9 +79,162 @@ describe('Option Combinations - ', () => {
         const result = await testPadding(50);
         await page.screenshot({ path: __dirname + '/screenshots/padding-with-justified-text.png' });
         result.forEach((item) => {
-            expect(item.width).toBeGreaterThanOrEqual(380);
+            expect(item.width).toBeGreaterThanOrEqual(370);
             expect(item.width).toBeLessThanOrEqual(400);
         })
+
+    })
+
+    test('Padding and align left', async () => {
+
+        const testPadding = async (p) => await page.evaluate((text, config, p) => {
+
+            const options = {
+                ...config.options,
+                align: 'left',
+                padding: 50,
+                style: {
+                    ...config.options.style
+                }
+            }
+
+            const elemAttributes = {
+                ...config.elemAttributes,
+                id: 'rect',
+                x: 0,
+                y: 0,
+                height: 500,
+                width: 500
+            }
+
+            const svg = document.querySelector('svg');
+            const elem = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            Object.entries(elemAttributes).forEach(([key, value]) => {
+                elem.setAttribute(key, value)
+            });
+
+            svg.appendChild(elem);
+
+            textInShape(text, elem, options);
+
+            const textGrp = document.getElementById('text-in-' + elemAttributes.id);
+
+            return Array.from(textGrp.children).map(e => {
+                const bbox = e.getBBox();
+                const { x, y, width } = bbox;
+                return { x, y, width };
+            });
+
+        }, text, config, p)
+
+        const result = await testPadding(50);
+        await page.screenshot({ path: __dirname + '/screenshots/padding-with-align-left.png' });
+        result.forEach((item) => {
+            expect(item.x).toBeLessThanOrEqual(52);
+
+        })
+
+    })
+
+    test('Padding and align right', async () => {
+
+        const testPadding = async (p) => await page.evaluate((text, config, p) => {
+
+            const options = {
+                ...config.options,
+                align: 'right',
+                padding: 50,
+                style: {
+                    ...config.options.style
+                }
+            }
+
+            const elemAttributes = {
+                ...config.elemAttributes,
+                id: 'rect',
+                x: 0,
+                y: 0,
+                height: 500,
+                width: 500
+            }
+
+            const svg = document.querySelector('svg');
+            const elem = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            Object.entries(elemAttributes).forEach(([key, value]) => {
+                elem.setAttribute(key, value)
+            });
+
+            svg.appendChild(elem);
+
+            textInShape(text, elem, options);
+
+            const textGrp = document.getElementById('text-in-' + elemAttributes.id);
+
+            return Array.from(textGrp.children).map(e => {
+                const bbox = e.getBBox();
+                const { x, y, width } = bbox;
+                return { x, y, width };
+            });
+
+        }, text, config, p)
+
+        const result = await testPadding(50);
+        await page.screenshot({ path: __dirname + '/screenshots/padding-with-align-right.png' });
+        result.forEach((item) => {
+            expect(item.x + item.width).toBeGreaterThanOrEqual(448);
+            expect(item.x + item.width).toBeLessThanOrEqual(450);
+        })
+
+    })
+
+    test('Padding and align center', async () => {
+
+        const result = await page.evaluate((text, config) => {
+
+            const options = {
+                ...config.options,
+                align: 'center',
+                padding: 50,
+                style: {
+                    ...config.options.style
+                }
+            }
+
+            const elemAttributes = {
+                ...config.elemAttributes,
+                id: 'rect',
+                x: 0,
+                y: 0,
+                height: 500,
+                width: 500
+            }
+
+            const svg = document.querySelector('svg');
+            const elem = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            Object.entries(elemAttributes).forEach(([key, value]) => {
+                elem.setAttribute(key, value)
+            });
+
+            svg.appendChild(elem);
+
+            textInShape(text, elem, options);
+
+            const textGrp = document.getElementById('text-in-' + elemAttributes.id);
+
+            return Array.from(textGrp.children).map(e => {
+                const bbox = e.getBBox();
+                const { x, y, width } = bbox;
+                return { x, y, width };
+            });
+
+        }, text, config)
+
+        await page.screenshot({ path: __dirname + '/screenshots/padding-with-align-center.png' });
+        result.forEach((item) => {
+            expect(item.x + (item.width / 2)).toBeGreaterThanOrEqual(248);
+            expect(item.x + (item.width / 2)).toBeLessThanOrEqual(252);
+        })
+
 
     })
 
